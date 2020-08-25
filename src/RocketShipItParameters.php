bece7ba60fafa15dc726af2b32f2722d2bd21ee7
Jake Richhart <jake@jakerichhart.com>
2020-08-25 11:54:18 -0400

    add RocketShipItParameters with some constants for parameter values

---
+++ src/RocketShipItParameters.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DoubleOh13\RocketShipIt;
+
+class RocketShipItParameters
+{
+    const PACKAGING_TYPE_UPS_LETTER = '01';
+    const PACKAGING_TYPE_UPS_YOUR_PACKING = '02';
+    const PACKAGING_TYPE_UPS_TUBE = '03';
+    const PACKAGING_TYPE_UPS_PAK = '04';
+    const PACKAGING_TYPE_UPS_EXPRESS_BOX = '21';
+    const PACKAGING_TYPE_UPS_25KG_BOX = '24';
+    const PACKAGING_TYPE_UPS_10KG_BOX = '25';
+    const PACKAGING_TYPE_UPS_PALLET = '30';
+    const PACKAGING_TYPE_UPS_SMALL_EXPRESS_BOX = '2a';
+    const PACKAGING_TYPE_UPS_MEDIUM_EXPRESS_BOX = '2b';
+    const PACKAGING_TYPE_UPS_LARGE_EXPRESS_BOX = '2c';
+    const PACKAGING_TYPE_UPS_FLATS = '56';
+    const PACKAGING_TYPE_UPS_PARCELS = '57';
+    const PACKAGING_TYPE_UPS_BPM = '58';
+    const PACKAGING_TYPE_FEDEX_EXTRA_LARGE_BOX = 'FEDEX_EXTRA_LARGE_BOX';
+    const FEDEX_LARGE_BOX = 'FEDEX_LARGE_BOX';
+    const FEDEX_MEDIUM_BOX = 'FEDEX_MEDIUM_BOX';
+    const FEDEX_SMALL_BOX = 'FEDEX_SMALL_BOX';
+    const FEDEX_10KG_BOX = 'FEDEX_10KG_BOX';
+    const FEDEX_25KG_BOX = 'FEDEX_25KG_BOX';
+    const FEDEX_BOX = 'FEDEX_BOX';
+    const FEDEX_ENVELOPE = 'FEDEX_ENVELOPE';
+    const FEDEX_PAK = 'FEDEX_PAK';
+    const FEDEX_TUBE = 'FEDEX_TUBE';
+    const FEDEX_YOUR_PACKAGING = 'YOUR_PACKAGING';
+    const STAMPS_LETTER = 'Letter';
+    const STAMPS_LARGE_ENVELOPE_OR_FLAT = 'Large Envelope or Flat';
+    const STAMPS_THICK_ENVELOPE = 'Thick Envelope';
+    const STAMPS_FLAT_RATE_PADDED_ENVELOPE = 'Flat Rate Padded Envelope';
+    const STAMPS_PACKAGE = 'Package';
+    const STAMPS_FLAT_RATE_BOX = 'Flat Rate Box';
+    const STAMPS_SMALL_FLAT_RATE_BOX = 'Small Flat Rate Box';
+    const STAMPS_LARGE_FLAT_RATE_BOX = 'Large Flat Rate Box';
+    const STAMPS_FLAT_RATE_ENVELOPE = 'Flat Rate Envelope';
+    const STAMPS_LARGE_PACKAGE = 'Large Package';
+    const STAMPS_OVERSIZED_PACKAGE = 'Oversized Package';
+    const STAMPS_REGIONAL_RATE_BOX_A = 'Regional Rate Box A';
+    const STAMPS_REGIONAL_RATE_BOX_B = 'Regional Rate Box B';
+    const STAMPS_REGIONAL_RATE_BOX_C = 'Regional Rate Box C';
+}
